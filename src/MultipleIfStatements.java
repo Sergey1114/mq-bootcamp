d68674c4c389cf9941e960b50e8eb099119883d6
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 18:21:32 +0400

    Even or Odd

---
+++ src/MultipleIfStatements.java
@@ -0,0 +1,10 @@
+public class MultipleIfStatements {
+    public static void main(String[] args) {
+        int num = 19;
+        if (num % 2 == 1) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
