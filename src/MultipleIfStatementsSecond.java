e8e47b554779bc8e2124dfe6d0daf8d16eba5f6e
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 20:37:51 +0400

    Traffic Light

--- src/MultipleIfStatementsSecond.java
+++ src/MultipleIfStatementsSecond.java
@@ -1,18 +1,12 @@
 public class MultipleIfStatementsSecond {
     public static void main(String[] args) {
-        int letter = 20;
-        if (letter >= 0 && letter <= 20) {
-            System.out.println("A");
-        } else if (letter > 20 && letter <= 40) {
-            System.out.println("B");
-        } else if (letter > 40 && letter <= 60) {
-            System.out.println("C");
-        } else if (letter > 60 && letter <= 80) {
-            System.out.println("D");
-        } else if (letter > 80 && letter <= 100) {
-            System.out.println("F");
+        String light = "green";
+        if (light == "yellow") {
+            System.out.println("Ready");
+        } else if (light == "green") {
+            System.out.println("Go");
+        } else if (light == "red") {
+            System.out.println("Stop");
         }
     }
 }
-
-
