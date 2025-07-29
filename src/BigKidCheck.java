53686f71d1f4a2880874ae8eb2d3ae5de853d9bc
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 17:09:47 +0400

    Big Kid Check

---
+++ src/BigKidCheck.java
@@ -0,0 +1,8 @@
+public class BigKidCheck {
+    public static void main(String[] args) {
+        int age = 10;
+        if (age >= 8) {
+            System.out.println("Big kid");
+        }
+    }
+}
