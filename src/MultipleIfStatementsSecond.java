a516bd466b6401d8b9904fff9c036451d716bea9
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 20:54:21 +0400

    Temperature Feeling

--- src/MultipleIfStatementsSecond.java
+++ src/MultipleIfStatementsSecond.java
@@ -1,12 +1,14 @@
 public class MultipleIfStatementsSecond {
     public static void main(String[] args) {
-        String light = "green";
-        if (light == "yellow") {
-            System.out.println("Ready");
-        } else if (light == "green") {
-            System.out.println("Go");
-        } else if (light == "red") {
-            System.out.println("Stop");
+        int temp = 15;
+        if (temp < 0) {
+            System.out.println("Very Cold");
+        } else if (temp < 15) {
+            System.out.println("Cold");
+        } else if (temp < 25) {
+            System.out.println("Warm");
+        }else{
+            System.out.println("Hot");
         }
     }
 }
