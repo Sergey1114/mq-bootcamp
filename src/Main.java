8ae0ca263dfc36157fbb857d4015bdf74a82fe6a
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 13:01:32 +0400

    Quotation Marks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("J");
-        System.out.println("A");
-        System.out.println("V");
-        System.out.println("A");
-        System.out.println("!");
+        System.out.println("\"Java is fun!\"");
     }
 }
