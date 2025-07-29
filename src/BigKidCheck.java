570ab095311aad77a0b23fc03cbabd74b1d630c9
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 17:34:29 +0400

    Perfect Score

--- src/BigKidCheck.java
+++
@@ -1,8 +0,0 @@
-public class BigKidCheck {
-    public static void main(String[] args) {
-        int temp = 5;
-        if(temp < 10) {
-            System.out.println("Wear a coat!");
-        }
-    }
-}
---
+++ src/SingleIfStatements.java
@@ -0,0 +1,8 @@
+public class SingleIfStatements {
+    public static void main(String[] args) {
+        int score = 100;
+        if(score == 100) {
+            System.out.println("You aced it!");
+        }
+    }
+}
