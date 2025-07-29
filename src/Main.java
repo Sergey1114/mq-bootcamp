ece98c0b4a6f1a380f62047d3d706167f5f9f82e
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 12:40:45 +0400

    Java Cheer

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("   /\\  ");
-        System.out.println("  /  \\");
-        System.out.println(" /____\\");
+        System.out.println("J");
+        System.out.println("A");
+        System.out.println("V");
+        System.out.println("A");
+        System.out.println("!");
     }
 }
