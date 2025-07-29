642f11420262a65de1066a85915957f354a47170
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-29 19:39:41 +0400

    Plant Watering

--- src/MultipleIfStatements.java
+++ src/MultipleIfStatements.java
@@ -1,10 +1,10 @@
 public class MultipleIfStatements {
     public static void main(String[] args) {
-        boolean finished = true ;
-        if (finished) {
-            System.out.println("Play!");
+        int daysDry = 4;
+        if (daysDry > 3) {
+            System.out.println("Water plant");
         } else {
-            System.out.println("Keep working");
+            System.out.println("Still moist");
         }
     }
 }
