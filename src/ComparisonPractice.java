b8659492f85a7dfd2727b1ad6deb08381b2af958
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 13:45:38 +0400

    Higher Grade

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,12 +1,13 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int num1 = -5;
-        int num2 = 2;
-        if (num1 < num2) {
-            System.out.println(num1 + " is smaller");
-        } else if (num2 < num1) {
-            System.out.println(num2 + "is smaller");
+        int score1 = 37;
+        int score2 = 45;
+        if (score1 > score2) {
+            System.out.println(score1 + " is bigger than " + score2);
+        } else if (score2 > score1) {
+            System.out.println(score2 + " is bigger than " + score1);
+        } else if (score1 == score2) {
+            System.out.println(score1 + " is equal to " + score2);
         }
     }
 }
-
