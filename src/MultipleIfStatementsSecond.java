50e31a43d1f7fb87d2711ac79627c2d220781828
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 11:21:34 +0400

    Number Sign

--- src/MultipleIfStatementsSecond.java
+++ src/MultipleIfStatementsSecond.java
@@ -1,14 +1,12 @@
 public class MultipleIfStatementsSecond {
     public static void main(String[] args) {
-        int temp = 15;
-        if (temp < 0) {
-            System.out.println("Very Cold");
-        } else if (temp < 15) {
-            System.out.println("Cold");
-        } else if (temp < 25) {
-            System.out.println("Warm");
-        }else{
-            System.out.println("Hot");
+        int num = 27;
+        if (num < 0) {
+            System.out.println("Negative");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        } else if (num > 0) {
+            System.out.println("Positive");
         }
     }
 }
