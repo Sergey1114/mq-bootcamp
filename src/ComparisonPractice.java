ce6dd3d4d351c0e177a858ab4da4ad3e2b6743f5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 13:24:37 +0400

    Largest of Two

---
+++ src/ComparisonPractice.java
@@ -0,0 +1,12 @@
+public class ComparisonPractice {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 12;
+        if (num1 > num2) {
+            System.out.println(num1 + " is bigger");
+        } else if (num2 > num1) {
+            System.out.println(num2 + "is bigger");
+        }
+    }
+}
+
