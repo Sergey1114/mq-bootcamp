16192f0980c7646b5eba249d86fcf96cb4f4f5c5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 12:59:46 +0400

    Exam Medal

--- src/MultipleIfStatementsSecond.java
+++ src/MultipleIfStatementsSecond.java
@@ -1,16 +1,15 @@
 public class MultipleIfStatementsSecond {
     public static void main(String[] args) {
-        int age = 21;
-        if (age > 0 && age <= 2) {
-            System.out.println("Baby");
-        } else if (age >= 3 && age <= 5) {
-            System.out.println("Toddler");
-        } else if (age >= 6 && age <= 12) {
-            System.out.println("Kid");
-        } else if (age >= 13 && age <= 17) {
-            System.out.println("Teen");
+        int score = 58;
+        if (score >= 90) {
+            System.out.println("Gold");
+        } else if (score >= 75) {
+            System.out.println("Silver");
+        } else if (score >= 60) {
+            System.out.println("Bronze");
         } else {
-            System.out.println("Adult");
+            System.out.println("None");
         }
     }
 }
+
