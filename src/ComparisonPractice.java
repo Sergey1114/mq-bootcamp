5bb1b4966781fd745b404451a0c31b593164b792
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 13:26:09 +0400

    Smallest of Two

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,11 +1,11 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int num1 = 8;
-        int num2 = 12;
-        if (num1 > num2) {
-            System.out.println(num1 + " is bigger");
-        } else if (num2 > num1) {
-            System.out.println(num2 + "is bigger");
+        int num1 = -5;
+        int num2 = 2;
+        if (num1 < num2) {
+            System.out.println(num1 + " is smaller");
+        } else if (num2 < num1) {
+            System.out.println(num2 + "is smaller");
         }
     }
 }
