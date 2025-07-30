ef10a3926afb7bf706291af200f9bf1b3b3060ca
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-07-30 12:32:21 +0400

    Age Group

--- src/MultipleIfStatementsSecond.java
+++ src/MultipleIfStatementsSecond.java
@@ -1,15 +1,16 @@
 public class MultipleIfStatementsSecond {
     public static void main(String[] args) {
-        int month = 10;
-        if (month == 12 || month == 1 || month == 2) {
-            System.out.println("Winter");
-        } else if (month == 3 || month == 4 || month == 5) {
-            System.out.println("Spring");
-        } else if (month == 6 || month == 7 || month == 8) {
-            System.out.println("Summer");
-        } else if (month == 9 || month == 10 || month == 11) {
-            System.out.println("Autumn");
+        int age = 21;
+        if (age > 0 && age <= 2) {
+            System.out.println("Baby");
+        } else if (age >= 3 && age <= 5) {
+            System.out.println("Toddler");
+        } else if (age >= 6 && age <= 12) {
+            System.out.println("Kid");
+        } else if (age >= 13 && age <= 17) {
+            System.out.println("Teen");
+        } else {
+            System.out.println("Adult");
         }
     }
 }
-
