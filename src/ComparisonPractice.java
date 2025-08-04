b095b39bb30443b440fffd7892ec7cb9bb6f3963
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 19:24:42 +0400

    Teen Checker

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,16 +1,10 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int a =4;
-        int b = 10;
-        int c = 7;
-        if (a < b && a < c) {
-            System.out.println("a is the smallest");
-        } else if (a > b && b < c) {
-            System.out.println("b is the smallest");
-        } else if (a > c && c < b) {
-            System.out.println("c is the smallest");
-        } else if (a == b && b == c) {
-            System.out.println("equal");
+        int age = 13;
+        if (age >= 13 && age <= 19) {
+            System.out.println("Teen");
+        } else {
+            System.out.println("Not a teen");
         }
     }
 }
