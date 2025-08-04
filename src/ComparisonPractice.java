2cc73b51634482689fba643a05b8b435b17758b5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 20:06:23 +0400

    Candy Jar Guess

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -4,11 +4,11 @@ import java.util.Random;
 public class ComparisonPractice {
     public static void main(String[] args) {
         Random random = new Random();
-        boolean heads = random.nextBoolean();
-        if (heads) {
-            System.out.println("Woo-hoo");
+        int num = random.nextInt( 30 - 10 + 1) + 10;
+        if (num > 20) {
+            System.out.println("Sugar Rush!");
         } else {
-            System.out.println("Darn");
+            System.out.println("Moderate snack.");
         }
     }
 }
