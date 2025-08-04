9c7ffa8ec498769135dc5d32fa4f0c1c69f39dcc
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 19:16:21 +0400

    Smallest of Three

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,13 +1,16 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int a = 6;
-        int b = 9;
-        if (a > b) {
-            System.out.println("a is bigger");
-        } else if (a == b) {
+        int a =4;
+        int b = 10;
+        int c = 7;
+        if (a < b && a < c) {
+            System.out.println("a is the smallest");
+        } else if (a > b && b < c) {
+            System.out.println("b is the smallest");
+        } else if (a > c && c < b) {
+            System.out.println("c is the smallest");
+        } else if (a == b && b == c) {
             System.out.println("equal");
-        } else {
-            System.out.println("b is bigger");
         }
     }
 }
