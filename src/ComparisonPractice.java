ffb292fab5d7e75b4c2d4a4445460c193ff9c4f3
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 17:24:22 +0400

    Equal Numbers?

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,14 +1,12 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int a = 9;
-        int b = 12;
-        int num = 10;
-        if ((a - num) < (b - num)) {
-            System.out.println("a is closer");
-        } else if ((a - num) == (b - num)) {
-            System.out.println("equal");
+        int a = 4;
+        int b = 3;
+        int result = 12;
+        if (a * b == 12) {
+            System.out.println("correct");
         } else {
-            System.out.println("b is closer");
+            System.out.println("incorrect");
         }
     }
 }
