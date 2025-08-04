97ef2c21f96d071a2f7b00a08fc082d81794dd97
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 19:42:57 +0400

    Lucky Dice

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,10 +1,14 @@
+import javax.naming.NamingEnumeration;
+import java.util.Random;
+
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int age = 13;
-        if (age >= 13 && age <= 19) {
-            System.out.println("Teen");
+        Random random = new Random();
+        int number = random.nextInt(6) + 1;
+        if (number == 6) {
+            System.out.println("Lucky");
         } else {
-            System.out.println("Not a teen");
+            System.out.println(number);
         }
     }
 }
