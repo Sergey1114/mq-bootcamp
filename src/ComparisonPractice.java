73260e760706eef5dd3efd574676f878413e4cd7
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 16:52:48 +0400

    Closest to Ten

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,14 +1,14 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int time1 = 30;
-        int time2 = 28;
-        int time3 = 29;
-        if (time1 <= time2 && time1 <= time3) {
-            System.out.println("runner 1 is the fastest");
-        } else if (time2 <= time1 && time2 <= time3) {
-            System.out.println("runner 2 is the fastest");
+        int a = 9;
+        int b = 12;
+        int num = 10;
+        if ((a - num) < (b - num)) {
+            System.out.println("a is closer");
+        } else if ((a - num) == (b - num)) {
+            System.out.println("equal");
         } else {
-            System.out.println("runner 3 is the fastest");
+            System.out.println("b is closer");
         }
     }
 }
