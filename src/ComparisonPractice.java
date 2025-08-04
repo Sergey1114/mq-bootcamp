5ec1e8e2cc6cbd72e3beb4841cdee483bac3d5c5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 17:40:10 +0400

    Double Check

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,12 +1,13 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int a = 4;
-        int b = 3;
-        int result = 12;
-        if (a * b == 12) {
-            System.out.println("correct");
+        int num1 = 2;
+        int num2 = 15;
+        if (num1 * num2 > 20) {
+            System.out.println("Big");
+        } else if (num1 * num2 == 20) {
+            System.out.println("Equal");
         } else {
-            System.out.println("incorrect");
+            System.out.println("Small");
         }
     }
 }
