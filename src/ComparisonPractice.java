b04fd4d64edf1f87b938b9c215a20b0d07a25b93
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-04 19:05:35 +0400

    Bigger of Two

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -1,10 +1,13 @@
 public class ComparisonPractice {
     public static void main(String[] args) {
-        int year = 2020;
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println("Leap year");
+        int a = 6;
+        int b = 9;
+        if (a > b) {
+            System.out.println("a is bigger");
+        } else if (a == b) {
+            System.out.println("equal");
         } else {
-            System.out.println("Ordinary year");
+            System.out.println("b is bigger");
         }
     }
 }
