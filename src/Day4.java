bd5e726780f996d26e768280223f32b6515c077f
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 11:31:14 +0400

    BMI Category

--- src/Day4.java
+++ src/Day4.java
@@ -1,16 +1,14 @@
 public class Day4 {
     public static void main(String[] args) {
-        int score = 75;
-        if (score > 0 && score <= 20) {
-            System.out.println("A");
-        } else if (score > 20 && score <= 40) {
-            System.out.println("B");
-        } else if (score > 40 && score <= 60) {
-            System.out.println("C");
-        } else if (score > 60 && score <= 80) {
-            System.out.println("D");
-        } else if (score > 80 && score <= 100) {
-            System.out.println("F");
+        double bmi = 24;
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 25) {
+            System.out.println("Normal");
+        } else if (bmi < 30) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
         }
     }
 }
