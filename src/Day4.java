8e9b13f0d30c9724f39d776cb370d950446673f4
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 12:26:08 +0400

    Triangle Type

--- src/Day4.java
+++ src/Day4.java
@@ -3,15 +3,19 @@ import java.util.Random;
 public class Day4 {
     public static void main(String[] args) {
         Random random = new Random();
-       int hour = random.nextInt(0,23);
-       if (hour >= 5 && hour <= 11) {
-           System.out.println("Morning");
-       } else if (hour > 11 && hour <= 16) {
-           System.out.println("Afternoon");
-       } else if (hour > 16 && hour <= 20) {
-           System.out.println("Evening");
-       } else {
-           System.out.println("Night");
-       }
+        int a = random.nextInt(0, 20);
+        int b = random.nextInt(0, 20);
+        int c = random.nextInt(0, 20);
+        System.out.println(a + " " + b + " " + c);
+
+        if (a + b > c && a + c > b && b + c > a) {
+            if (a == b && b == c) {
+                System.out.println("Equilateral");
+            } else if (a == b || a == c || b == c) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        }
     }
 }
