a44a45514a8a3d847330a9caba483cb06b27219b
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 10:38:10 +0400

    Even or Odd

--- src/Day4.java
+++ src/Day4.java
@@ -1,8 +1,10 @@
 public class Day4 {
     public static void main(String[] args) {
-        int tempFar = 62;
-        int tempCel = tempFar - 32;
-        System.out.println(tempFar);
-        System.out.println(tempCel);
+        int x = 17;
+        if (x % 2 == 0) {
+            System.out.println("odd");
+        } else {
+            System.out.println("even");
+        }
     }
 }
