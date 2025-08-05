bde6ff689f218f6058b69eec6be9c49f68d10645
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 11:42:13 +0400

    Day Part

--- src/Day4.java
+++ src/Day4.java
@@ -1,14 +1,17 @@
+import java.util.Random;
+
 public class Day4 {
     public static void main(String[] args) {
-        double bmi = 24;
-        if (bmi < 18.5) {
-            System.out.println("Underweight");
-        } else if (bmi < 25) {
-            System.out.println("Normal");
-        } else if (bmi < 30) {
-            System.out.println("Overweight");
-        } else {
-            System.out.println("Obese");
-        }
+        Random random = new Random();
+       int hour = random.nextInt(0,23);
+       if (hour >= 5 && hour <= 11) {
+           System.out.println("Morning");
+       } else if (hour > 11 && hour <= 16) {
+           System.out.println("Afternoon");
+       } else if (hour > 16 && hour <= 20) {
+           System.out.println("Evening");
+       } else {
+           System.out.println("Night");
+       }
     }
 }
