882e0a43b4db273ee06d6d1338d353ba22f0e2b7
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 11:06:05 +0400

    Letter Grade

--- src/Day4.java
+++ src/Day4.java
@@ -1,10 +1,16 @@
 public class Day4 {
     public static void main(String[] args) {
-        int speed = 102;
-        if (speed < 90) {
-            System.out.println("OK");
-        } else {
-            System.out.println("Speeding Fine!");
+        int score = 75;
+        if (score > 0 && score <= 20) {
+            System.out.println("A");
+        } else if (score > 20 && score <= 40) {
+            System.out.println("B");
+        } else if (score > 40 && score <= 60) {
+            System.out.println("C");
+        } else if (score > 60 && score <= 80) {
+            System.out.println("D");
+        } else if (score > 80 && score <= 100) {
+            System.out.println("F");
         }
     }
 }
