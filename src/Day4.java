f1bf43adc9dab45645182a881ffbff5b2d2d0280
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 12:43:07 +0400

    Tax Bracket

--- src/Day4.java
+++ src/Day4.java
@@ -3,19 +3,15 @@ import java.util.Random;
 public class Day4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int a = random.nextInt(0, 20);
-        int b = random.nextInt(0, 20);
-        int c = random.nextInt(0, 20);
-        System.out.println(a + " " + b + " " + c);
-
-        if (a + b > c && a + c > b && b + c > a) {
-            if (a == b && b == c) {
-                System.out.println("Equilateral");
-            } else if (a == b || a == c || b == c) {
-                System.out.println("Isosceles");
-            } else {
-                System.out.println("Scalene");
-            }
+        double income = random.nextDouble(100500);
+        if (income < 10000) {
+            System.out.println("Low");
+        } else if (income < 30000) {
+            System.out.println("Medium");
+        } else if (income < 100000) {
+            System.out.println("High");
+        } else {
+            System.out.println("Very High");
         }
     }
 }
