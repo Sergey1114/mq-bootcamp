6a22e3d0184351fff0ad8e6e1b66af83bf3cf2c5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 16:26:06 +0400

    Coin Flip

--- src/Day4.java
+++ src/Day4.java
@@ -3,7 +3,11 @@ import java.util.Random;
 public class Day4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int dice = random.nextInt(6) + 1;
-        System.out.println("You rolled " + dice);
+        int flip = random.nextInt(2);
+        if (flip == 0) {
+            System.out.println("Heads");
+        } else {
+            System.out.println("Tails");
+        }
     }
 }
