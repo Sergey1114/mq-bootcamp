4c949730087838a71289382c89c9849c624709ab
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 16:19:22 +0400

    Dice Roll

--- src/Day4.java
+++ src/Day4.java
@@ -3,27 +3,7 @@ import java.util.Random;
 public class Day4 {
     public static void main(String[] args) {
         Random random = new Random();
-        char symbol = (char) random.nextInt(42, 48);
-        double num1 = random.nextDouble(100);
-        double num2 = random.nextDouble(100);
-
-        System.out.println(symbol);
-        System.out.println(num1);
-        System.out.println(num2);
-
-        switch (symbol) {
-            case '+':
-                System.out.println(num1 + num2);
-                break;
-            case '-':
-                System.out.println(num1 - num2);
-                break;
-            case '*':
-                System.out.println(num1 * num2);
-                break;
-            case '/':
-                System.out.println(num1 / num2);
-
-        }
+        int dice = random.nextInt(6) + 1;
+        System.out.println("You rolled " + dice);
     }
 }
