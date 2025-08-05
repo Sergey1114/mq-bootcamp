57ac17f6401eab6047503700b8d2f858c250828d
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 17:14:54 +0400

    Password

--- src/Day4.java
+++ src/Day4.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class Day4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int R = random.nextInt(0, 256);
-        int G = random.nextInt(0, 256);
-        int B = random.nextInt(0, 256);
-        System.out.println("(" + R + "," + G + "," + B + ")");
+        char ch1 =(char) random.nextInt(97, 123);
+        char ch2 =(char) random.nextInt(97, 123);
+        char ch3 =(char) random.nextInt(97, 123);
+        System.out.println(ch1 + "," + ch2 + "," + ch3);
     }
 }
