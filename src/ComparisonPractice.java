54af7f8cdd5661241ede49c578b283ea0182c64a
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-05 10:39:03 +0400

    Even or Odd

--- src/ComparisonPractice.java
+++ src/ComparisonPractice.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class ComparisonPractice {
     public static void main(String[] args) {
         Random random = new Random();
-        int num = random.nextInt( 30 - 10 + 1) + 10;
+        int num = random.nextInt(30 - 10 + 1) + 10;
         if (num > 20) {
             System.out.println("Sugar Rush!");
         } else {
