d3429822fa9b0c760ca1df9a2568c84784c87954
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 15:21:13 +0400

    Greeting Switch

--- src/Day5.java
+++ src/Day5.java
@@ -3,8 +3,17 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String s = "He said " + "\"OK\"";
-        System.out.println(s);
-
+        String lang = "fr";
+        switch (lang) {
+            case "en":
+                System.out.println("Hello");
+                break;
+            case "fr":
+                System.out.println("Bonjour");
+                break;
+            case "es":
+                System.out.println("Hola");
+                break;
+        }
     }
 }
