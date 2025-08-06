7610c64516003d54eeb26adf252e5675c7d60f52
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 11:35:19 +0400

    First Equals Last

--- src/Day5.java
+++ src/Day5.java
@@ -3,11 +3,9 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String ch = "A";
-        if (ch.equals(ch.toLowerCase())) {
-            System.out.println(ch.toLowerCase());
-        } else if (ch.equals(ch.toUpperCase())){
-            System.out.println(ch.toUpperCase());
-        }
+        String word = "level";
+        char first = word.charAt(0);
+        char last = word.charAt(4);
+        System.out.println(first == last);
     }
 }
