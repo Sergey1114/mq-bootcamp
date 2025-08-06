d8d9861e088026dfd392e174605c0f2103a45aa8
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 15:49:44 +0400

    Uppercase Ticket

--- src/Day5.java
+++ src/Day5.java
@@ -4,10 +4,7 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        Random random = new Random();
-        int a = random.nextInt(0, 10);
-        int b = random.nextInt(0, 10);
-        String r = "" + a + b;
-        System.out.println(r);
+        String title = "Avengers";
+        System.out.println(title.toUpperCase());
     }
 }
