1a52343192aa041d6e4af31439bbbb0d32ebd02e
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 11:23:56 +0400

    Case Flipper

--- src/Day5.java
+++ src/Day5.java
@@ -3,8 +3,11 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String word = "cat";
-        char letter = word.charAt(1);
-        System.out.println(letter);
+        String ch = "A";
+        if (ch.equals(ch.toLowerCase())) {
+            System.out.println(ch.toLowerCase());
+        } else if (ch.equals(ch.toUpperCase())){
+            System.out.println(ch.toUpperCase());
+        }
     }
 }
