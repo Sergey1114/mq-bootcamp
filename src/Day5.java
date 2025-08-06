53b4756ae331166e4d2107e6b71ac2cac8f8d128
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 10:39:58 +0400

    Name Length

--- src/Day5.java
+++ src/Day5.java
@@ -1,6 +1,7 @@
 public class Day5 {
     public static void main(String[] args) {
-        String quote = "\"Stay hungry, stay foolish\"";
-        System.out.println(quote);
+        String name1 = "Mher";
+        int name2 = name1.length();
+        System.out.println(name2);
     }
 }
