fb052db657b1e48be9601d52707d9371f30f03d9
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 13:57:32 +0400

    Space to Underscore

--- src/Day5.java
+++ src/Day5.java
@@ -3,8 +3,7 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String text = "I know that is a secret";
-        System.out.println(text.toLowerCase());
-        System.out.println(text.contains("secret"));
+        String a = "Hi, I love JAVA";
+        System.out.println(a.replace(" ", "*"));
     }
 }
