7fcd2ad605dbc07522fe2d53622972465b7dc707
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 17:01:30 +0400

    Char Code

--- src/Day5.java
+++ src/Day5.java
@@ -4,15 +4,9 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        String letter1 = "b";
-        String letter2 = "A";
-        int r = letter1.compareToIgnoreCase(letter2);
-        if (r < 0) {
-            System.out.println(letter1 + " comes before " + letter2);
-        } else if (r > 0) {
-            System.out.println(letter2 + " comes before " + letter1);
-        } else {
-            System.out.println(letter1 + " and " + letter2 + " are the same letter");
-        }
+        String str = "A";
+        char ch = str.charAt(0);
+        int code = ch;
+        System.out.println(code);
     }
 }
