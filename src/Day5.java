ade9841d5ed9c5179195e4b2f99e34e705e6bb9a
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 14:49:27 +0400

    Double Quote Escape

--- src/Day5.java
+++ src/Day5.java
@@ -3,9 +3,8 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String messy = "  hi  ";
-        System.out.println("Before trim: " + messy.length());
-        System.out.println("After trim: " + messy.trim().length());
-        System.out.println(messy.trim());
+        String s = "He said " + "\"OK\"";
+        System.out.println(s);
+
     }
 }
