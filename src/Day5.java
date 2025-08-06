bb19d39024e67518d74504a5b3de7594881e1796
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 16:51:21 +0400

    Alphabet Order

--- src/Day5.java
+++ src/Day5.java
@@ -4,8 +4,15 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        String a = new String("Sergey");
-        String b = new String("sergey");
-        System.out.println(a.equalsIgnoreCase(b));
+        String letter1 = "b";
+        String letter2 = "A";
+        int r = letter1.compareToIgnoreCase(letter2);
+        if (r < 0) {
+            System.out.println(letter1 + " comes before " + letter2);
+        } else if (r > 0) {
+            System.out.println(letter2 + " comes before " + letter1);
+        } else {
+            System.out.println(letter1 + " and " + letter2 + " are the same letter");
+        }
     }
 }
