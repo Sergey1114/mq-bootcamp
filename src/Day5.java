faff4b3903ed22207bf1b6d6c5495b7bc3cbd90b
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 12:26:40 +0400

    File Extension

--- src/Day5.java
+++ src/Day5.java
@@ -3,11 +3,8 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String email = "[a@b.com](mailto:a@b.com)";
-        if (email.contains("@") && email.contains(".")) {
-            System.out.println("valid");
-        } else {
-            System.out.println("invalid");
-        }
+        String fileName = "photo.jpeg";
+        String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+        System.out.println(extension);
     }
 }
