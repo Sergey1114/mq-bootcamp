bc89ed0c9e4467add4f40e1b4c2c5478f78faf89
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 10:57:18 +0400

    Initials Builder

--- src/Day5.java
+++ src/Day5.java
@@ -1,7 +1,13 @@
+import java.text.StringCharacterIterator;
+
 public class Day5 {
     public static void main(String[] args) {
-        String name1 = "Mher";
-        int name2 = name1.length();
-        System.out.println(name2);
+        String name1 = "Harry";
+        String name2 = "Potter";
+
+        char name1Initial = name1.charAt(0);
+        char name2Initial = name2.charAt(0);
+
+        System.out.println(name1Initial + "." + name2Initial + ".");
     }
 }
