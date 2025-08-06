65c2c031bb47fdb70985829745db9f45f91f4690
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 16:04:58 +0400

    Swap Halves

--- src/Day5.java
+++ src/Day5.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        String core = "cool";
-        String r = "pre-" + core + "-post";
-        System.out.println(r);
+        String word = "dusk";
+        String swap = word.substring(2) + word.substring(0, 2);
+        System.out.println(swap);
     }
 }
