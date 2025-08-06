1972bfba7d6608feced04c08945146d622d4cefb
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-06 11:02:37 +0400

    Last Three

--- src/Day5.java
+++ src/Day5.java
@@ -2,12 +2,7 @@ import java.text.StringCharacterIterator;
 
 public class Day5 {
     public static void main(String[] args) {
-        String name1 = "Harry";
-        String name2 = "Potter";
-
-        char name1Initial = name1.charAt(0);
-        char name2Initial = name2.charAt(0);
-
-        System.out.println(name1Initial + "." + name2Initial + ".");
+        String word = "elephant";
+        System.out.println(word.substring(word.length() - 3));
     }
 }
