580a669eda71cf1bfa8ba5cc1dc979d2cba688cd
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 11:05:02 +0400

    Even Numbers 2-100

--- src/Day6.java
+++ src/Day6.java
@@ -1,10 +1,10 @@
 public class Day6 {
     public static void main(String[] args) {
-        int sum = 0;
+        int number = 2;
 
-        for (int i = 1; i <= 100; i++) {
-            sum += i;
+        while (number <= 100) {
+            System.out.print(number + " ");
+            number += 2;
         }
-        System.out.println("Sum = " + sum);
     }
 }
