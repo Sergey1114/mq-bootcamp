9ac36d0985aba1827acae77229ec0efed5d94d04
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 17:32:01 +0400

    Sum of Digits

--- src/Day6.java
+++ src/Day6.java
@@ -2,14 +2,15 @@ import java.util.Random;
 
 public class Day6 {
     public static void main(String[] args) {
-        int number = 123456;
-        int reversed = 0;
+        int number = 2415;  // You can change this to any number
+        int sum = 0;
 
-        while (number != 0) {
-            int digit = number % 10;
-            reversed = reversed * 10 + digit;
-            number = number / 10;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
         }
-        System.out.println(reversed);
+
+        System.out.println("Digit sum = " + sum);
+
     }
 }
