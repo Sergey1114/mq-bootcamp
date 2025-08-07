7337dc08c6cfea680fec790c06a25ce1a7bf8971
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 13:53:06 +0400

    Guess Until Hit

--- src/Day6.java
+++ src/Day6.java
@@ -1,7 +1,23 @@
+import java.util.Random;
+
 public class Day6 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(5 * i);
+
+        Random random = new Random();
+        int secret = random.nextInt(20) + 1;
+        int iterator = 1;
+
+        while (true) {
+            int guess = random.nextInt(20) + 1;
+
+            if (secret != guess) {
+
+                iterator = iterator + 1;
+
+            } else {
+                System.out.println("Found " + secret + " in " + iterator + " guesses");
+                break;
+            }
         }
     }
 }
