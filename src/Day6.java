91acc15d08233edbb6a8b319012ed181b9bc9e30
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 10:48:35 +0400

    Sum 1-100

--- src/Day6.java
+++ src/Day6.java
@@ -1,7 +1,10 @@
 public class Day6 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
         }
+        System.out.println("Sum = " + sum);
     }
 }
