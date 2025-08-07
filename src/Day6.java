b85e543965a98c9ae7b8d7107c6148ee686e6e0a
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 14:26:11 +0400

    Countdown

--- src/Day6.java
+++ src/Day6.java
@@ -2,22 +2,9 @@ import java.util.Random;
 
 public class Day6 {
     public static void main(String[] args) {
-
-        Random random = new Random();
-        int secret = random.nextInt(20) + 1;
-        int iterator = 1;
-
-        while (true) {
-            int guess = random.nextInt(20) + 1;
-
-            if (secret != guess) {
-
-                iterator = iterator + 1;
-
-            } else {
-                System.out.println("Found " + secret + " in " + iterator + " guesses");
-                break;
-            }
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
         }
+        System.out.println("Blast off!");
     }
 }
