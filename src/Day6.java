c40aa3720c293ab718b25dd01325b4662a0bfc8e
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-07 11:20:33 +0400

    Multiplication Table

--- src/Day6.java
+++ src/Day6.java
@@ -1,10 +1,7 @@
 public class Day6 {
     public static void main(String[] args) {
-        int number = 2;
-
-        while (number <= 100) {
-            System.out.print(number + " ");
-            number += 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(5 * i);
         }
     }
 }
