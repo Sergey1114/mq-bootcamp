31575c88ed9bfcd495917759e6ea3ef78e952c6e
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-09 19:47:07 +0400

    Word Repeater

--- src/Day5.java
+++ src/Day5.java
@@ -4,11 +4,8 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        String link = "https://app.slack.com/client/T097XJ39MDG/C098C0WAM3K";
-        if (link.startsWith("https://")) {
-            System.out.println("https url");
-        } else {
-            System.out.println("not secure");
-        }
+        String base = "ha";
+        String result = base + base + base;
+        System.out.println(result);
     }
 }
