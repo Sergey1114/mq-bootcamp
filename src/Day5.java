4fbdee629458c8dc676984675bb2b1e9ea5247cf
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-09 19:53:44 +0400

    Reverse Two

--- src/Day5.java
+++ src/Day5.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-        String base = "ha";
-        String result = base + base + base;
-        System.out.println(result);
+        String xy = "ab";
+        String reversed = "" + xy.charAt(1) + xy.charAt(0);
+        System.out.println(reversed);
     }
 }
