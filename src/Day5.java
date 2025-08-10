7160860ace97364a8bf18a23a66bf9c3a6410309
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-10 15:33:46 +0400

    First Word

--- src/Day5.java
+++ src/Day5.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 public class Day5 {
     public static void main(String[] args) {
-
+        String phrase = "Hello world";
+        int spaceIndex = phrase.indexOf(' ');
+        String firstWord = phrase.substring(0, spaceIndex);
+        System.out.println(firstWord);
     }
 }
