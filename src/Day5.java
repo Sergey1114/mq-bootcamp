b037e9b4fedb6c56f1764e2a8ccc9252827a1bc5
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-10 15:41:08 +0400

    Last Word

--- src/Day5.java
+++ src/Day5.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Day5 {
     public static void main(String[] args) {
         String phrase = "Hello world";
-        int spaceIndex = phrase.indexOf(' ');
-        String firstWord = phrase.substring(0, spaceIndex);
-        System.out.println(firstWord);
+        int lastSpace = phrase.lastIndexOf(' ');
+        String lastWord = phrase.substring(lastSpace + 1);
+        System.out.println(lastWord);
     }
 }
