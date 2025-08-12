05d5c64b9c409598d492f85aecd2322385448e6c
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-12 13:38:13 +0400

    Mirror Word

--- src/Day7.java
+++ src/Day7.java
@@ -1,12 +1,11 @@
 public class Day7 {
     public static void main(String[] args) {
-        String snack = "chocolate";
-        for (int i = 0; i < snack.length(); i++) {
-            if (snack.charAt(i) == 'a') {
-                System.out.println(i);
-                return;
-            }
+        String w = "code";
+        String rev = "";
+        for (int i = w.length() - 1; i >= 0; i--) {
+            rev = rev + w.charAt(i);
         }
-        System.out.println(-1);
+        System.out.println(w);
+        System.out.println(rev);
     }
 }
