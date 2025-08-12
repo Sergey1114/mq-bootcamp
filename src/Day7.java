a7de685af4a419e3d4529ef782df686d25486ab2
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-12 11:25:16 +0400

    Whisper Mode

--- src/Day7.java
+++ src/Day7.java
@@ -1,6 +1,6 @@
 public class Day7 {
     public static void main(String[] args) {
-        String message = "We come in peace";
-        System.out.println(message.toUpperCase());
+        String message = "WE COME IN PEACE";
+        System.out.println(message.toLowerCase());
     }
 }
