a03a5d3e0039872248db391d2aa55d858eaad379
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-12 11:51:59 +0400

    Count Vowels in a Pet Name

--- src/Day7.java
+++ src/Day7.java
@@ -1,6 +1,14 @@
 public class Day7 {
     public static void main(String[] args) {
-        String sentence = "Hello, my name is Sergey";
-        System.out.println(sentence.replace(" " , "_"));
+        String name = "Banana";
+        int count = 0;
+        name = name.toLowerCase();
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
