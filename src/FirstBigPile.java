34b8c22aa9af1584911bdd8993a2b0e07c80bac1
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-12 18:56:28 +0400

    Skip Sour Candies

--- src/FirstBigPile.java
+++ src/FirstBigPile.java
@@ -1,15 +1,10 @@
 public class FirstBigPile {
     public static void main(String[] args) {
-        int[] boxes = {4, 5, 2, 1, 5, 9, 3};
-        int i;
-        for (i = 0; i < boxes.length; i++) {
-            if (boxes[i] > 5) {
-                System.out.println("First big pile: " + boxes[i] + " at index " + i);
-                break;
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4 || i == 7) {
+                continue;
             }
-        }
-        if (i == boxes.length) {
-            System.out.println("none");
+            System.out.print(i + " ");
         }
     }
 }
