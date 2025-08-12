b953c669be6466d55e5c221d20e1969105341dbb
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-12 16:32:06 +0400

    Season Finder

--- src/Day7.java
+++ src/Day7.java
@@ -2,32 +2,6 @@ import java.util.Random;
 
 public class Day7 {
     public static void main(String[] args) {
-        Random random = new Random();
-        int map = random.nextInt(1, 8);
-        switch (map) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("invalid");
-        }
+
     }
 }
