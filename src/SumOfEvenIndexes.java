c5c3f77e9b3d89b9e4306aa8df9133f9feb57335
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-13 13:50:53 +0400

    Sum of Even Indexes

---
+++ src/SumOfEvenIndexes.java
@@ -0,0 +1,10 @@
+public class SumOfEvenIndexes {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6}; // example array
+        int sum = 0;
+        for (int i = 0; i < a.length; i += 2) {
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+}
