8e612f651d2eb16407e2cae1a6579d51bd185f90
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-18 21:53:46 +0400

    Fill 1..N

---
+++ src/Fill1ToN.java
@@ -0,0 +1,9 @@
+public class Fill1ToN {
+    public static void main(String[] args) {
+        int[] a = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i + 1;
+            System.out.println(a[i]);
+        }
+    }
+}
