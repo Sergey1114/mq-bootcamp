db75cceb70fa1cc24846af969588d4d7716b2a4c
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-19 16:16:07 +0400

    areaRect Write

---
+++ src/P22.java
@@ -0,0 +1,10 @@
+public class P22 {
+    public static void main(String[] args) {
+        int result = areaRect(3, 4);
+        System.out.println(result);
+    }
+
+    public static int areaRect(int w, int h) {
+        return w * h;
+    }
+}
