57474216f66ec51f04d3cd5208d78b97fb9be7c8
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-19 15:57:51 +0400

    average3 Write

---
+++ src/P20.java
@@ -0,0 +1,10 @@
+public class P20 {
+    public static void main(String[] args) {
+        double result = average3(3, 5, 7);
+        System.out.println(result);
+    }
+
+    public static double average3(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+}
