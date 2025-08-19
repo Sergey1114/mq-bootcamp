803e04896d91102cf748b199714ca1d72bfede61
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-19 16:06:46 +0400

    perimeterRect Write

---
+++ src/P21.java
@@ -0,0 +1,10 @@
+public class P21 {
+    public static void main(String[] args) {
+        int result = perimeterRect(3, 4);
+        System.out.println(result);
+    }
+
+    public static int perimeterRect(int w, int h) {
+        return 2 * (3 + 4);
+    }
+}
