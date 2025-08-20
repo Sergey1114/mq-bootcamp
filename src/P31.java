23ff97864515672f50bef4b7e30c23c269e3b968
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-20 12:04:14 +0400

    shout Write

---
+++ src/P31.java
@@ -0,0 +1,10 @@
+public class P31 {
+    public static void main(String[] args) {
+        String result = shout("hi");
+        System.out.println(result);
+    }
+
+    public static String shout(String s) {
+        return s.toUpperCase();
+    }
+}
