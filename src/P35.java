48f9e851da4d2e72cf18221f94e51184ef781d27
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-20 12:26:58 +0400

    addQuotes Write

---
+++ src/P35.java
@@ -0,0 +1,10 @@
+public class P35 {
+    public static void main(String[] args) {
+        String result = addQuotes("Mher");
+        System.out.println(result);
+    }
+
+    public static String addQuotes(String s) {
+        return "\"Mher\"";
+    }
+}
