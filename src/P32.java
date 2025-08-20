ab46ff08762eb9a2b791ccbc9ead6e6b00fea696
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-20 12:06:40 +0400

    whisper Write

---
+++ src/P32.java
@@ -0,0 +1,10 @@
+public class P32 {
+    public static void main(String[] args) {
+        String result = shout("HELLO");
+        System.out.println(result);
+    }
+
+    public static String shout(String s) {
+        return s.toLowerCase();
+    }
+}
