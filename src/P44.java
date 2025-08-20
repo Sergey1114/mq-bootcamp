bc2074db88fa11c3293b83e2e265b627cc69fe02
Sergey Ghazaryan <sergeyghazaryan2014@gmail.com>
2025-08-20 13:55:47 +0400

    joinWithSpace Write

---
+++ src/P44.java
@@ -0,0 +1,10 @@
+public class P44 {
+    public static void main(String[] args) {
+        String result = joinWithSpace("hi", "there");
+        System.out.println(result);
+    }
+
+    public static String joinWithSpace(String a, String b) {
+        return a + " " + b;
+    }
+}
